bb5a0ad3ab74536a2ce116284383b0fb9c17593f
JAAG4 <45705167+JAAG4@users.noreply.github.com>
2025-05-23 02:15:45 -0400

    refactor: Function template and deleted unused code

--- include/Visualization.hpp
+++ include/Visualization.hpp
@@ -2,30 +2,6 @@
 #include "raylib.h"
 #include "resource_dir.h" // utility header for SearchAndSetResourceDir
 #include "Wfc3D.hpp"
-// std::vector<Color> colors = {LIGHTGRAY,
-//                              GRAY,
-//                              DARKGRAY,
-//                              YELLOW,
-//                              GOLD,
-//                              ORANGE,
-//                              PINK,
-//                              RED,
-//                              MAROON,
-//                              GREEN,
-//                              LIME,
-//                              DARKGREEN,
-//                              SKYBLUE,
-//                              BLUE,
-//                              DARKBLUE,
-//                              PURPLE,
-//                              VIOLET,
-//                              DARKPURPLE,
-//                              BEIGE,
-//                              BROWN,
-//                              DARKBROWN,
-//                              BLACK,
-//                              BLANK,
-//                              MAGENTA,
-//                              RAYWHITE};
 
 void display_scene_from_matrix(Wfc3D wfc3d, std::vector<Color> display_colors);
+void display_scene_from_matrix(std::vector<std::vector<std::vector<std::set<unsigned int>>>> wfc3d, std::vector<Color> display_colors);
